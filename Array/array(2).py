b36bcbb730d87c1a20aad849adb6e69dfe6dc6c1
Mantiquilla,JK <s.jkmmantiquilla@usm.edu.ph>
2025-02-10 21:14:52 +0800

    array

---
+++ Array/array(2).py
@@ -0,0 +1,8 @@
+years = ["2005", "2007", "2009", "2011"]
+print("Years after two years:", years)
+
+years.append("2013")
+print("Adding another two years:", years)
+
+years.remove("2009")
+print("Removing one of the years:", years)
---
+++ Array/array.py
@@ -0,0 +1,14 @@
+months = ["January", "February", "March", "April"]
+print("First month:", months[0])
+print("Second month:", months[1])
+
+months.append("May")
+print("New Month:", months)
+
+months.remove("January")
+print("Months after removing a month:", months)
+
+
+
+
+
